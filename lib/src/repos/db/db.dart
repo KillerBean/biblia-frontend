39e4fea7b18c13b2786f055652c2f2ccdcd77b70
Luis Roseno <nando.roseno@gmail.com>
2024-10-18 21:52:19 -0300

    reverte alteração da call databaseFactoryFfi

--- lib/src/repos/db/db.dart
+++ lib/src/repos/db/db.dart
@@ -17,7 +17,9 @@ class DatabaseRetriever {
     if (Platform.isWindows || Platform.isLinux) {
       // Initialize FFI
       sqfliteFfiInit();
+    }
 
+    if (!Platform.isAndroid && !Platform.isIOS) {
       // Change the default factory. On iOS/Android, if not using `sqlite_flutter_lib` you can forget
       // this step, it will use the sqlite version available on the system.
       databaseFactory = databaseFactoryFfi;

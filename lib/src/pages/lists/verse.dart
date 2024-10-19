9fbd887c8aca95acc6d5180a250b9e8b1191f6c7
Luis Roseno <nando.roseno@gmail.com>
2024-10-18 21:53:52 -0300

    cria pagina de verses

---
+++ lib/src/pages/lists/verse.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListVersesPage extends StatefulWidget {
+  const ListVersesPage({super.key});
+
+  @override
+  State<ListVersesPage> createState() => _ListVersesPageState();
+}
+
+class _ListVersesPageState extends State<ListVersesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

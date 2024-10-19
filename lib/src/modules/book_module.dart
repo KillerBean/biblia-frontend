9c82efd1421af5ebb5476bde6fed8d678f8ce389
Luis Roseno <nando.roseno@gmail.com>
2024-10-19 02:32:31 -0300

    adiciona pagina de capitulos

--- lib/src/modules/book_module.dart
+++ lib/src/modules/book_module.dart
@@ -1,5 +1,6 @@
 import 'package:biblia/src/pages/lists/book.dart';
 import 'package:biblia/src/pages/widgets/bookpage_widget.dart';
+import 'package:biblia/src/pages/widgets/chapterpage_widget.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class BookModule extends Module {
@@ -12,5 +13,9 @@ class BookModule extends Module {
     r.child('/:bookid',
         child: (context) =>
             BookPageWidget(bookId: int.parse(r.args.params["bookid"])));
+    r.child('/:bookid/:chapterid',
+        child: (context) => ChapterPageWidget(
+            bookId: int.parse(r.args.params["bookid"]),
+            chapterId: int.parse(r.args.params["chapterid"])));
   }
 }
